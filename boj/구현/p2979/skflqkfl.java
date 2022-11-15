e3bb6ef82f494ce9edd46c70547e963285058c8d
Park JunYoung <parkjun611@gmail.com>
2022-11-16 00:56:51 +0900

    AppConfig 리펙터링

--- boj/구현/p2979/skflqkfl.java
+++ boj/구현/p2979/skflqkfl.java
@@ -3,21 +3,92 @@ package boj.구현.p2979;
 import java.util.*;
 
 public class skflqkfl {
+
+    private static final int MIN_TIME = 1;
+    private static final int MAX_TIME = 100;
+    private static int COUNT = 3;
+    private static boolean log = false;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<Map<String, Object>> inputList = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            inputList.add(Map.of("price", scanner.nextInt()));
+        int[] price = new int[COUNT];
+        int[] cnt = new int[MAX_TIME];
+        int priceSum = 0;
+
+        for(int i=0; i<COUNT; i++) {
+            price[i] = scanner.nextInt();
         }
 
-//        for(int i=0; i<2; i++) {
-//            inputList.get(i).put("startTime", scanner.nextInt());
-//            inputList.get(i).put("endTime", scanner.nextInt());
-//        }
+        for(int i=0; i<COUNT; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            for(int j=a; j<b; j++) {
+                cnt[j]++;
+            }
+        }
 
-        inputList.get(0).get("price");
-//        inputList.get(0).put("startTime", 1);
+        for(int i=0; i<MAX_TIME; i++) {
+            if(cnt[i] > 0) {
+                priceSum += cnt[i] * price[cnt[i]-1];
+            }
+        }
 
-        inputList.forEach(m -> System.out.println(m.toString()));
+        System.out.println(priceSum);
     }
+
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        List<Integer> priceList = new ArrayList<>();
+//        List<Map<String, Object>> carList = new ArrayList<>();
+//        priceList.add(0);
+//        for (int i = 0; i < COUNT; i++) {
+//            priceList.add(scanner.nextInt());
+//        }
+//
+//        for (int i = 0; i < COUNT; i++) {
+//            Map<String, Object> map = new HashMap<>();
+//            map.put("startTime", scanner.nextInt());
+//            map.put("endTime", scanner.nextInt());
+//            carList.add(map);
+//        }
+//
+//        int minStartTime = minStartTime(carList);
+//        int maxEndTime = maxEndTime(carList);
+//        int priceSum = 0;
+//        int carCount = 0;
+//
+//        for (int i = minStartTime; i < maxEndTime; i++) {
+//            carCount = 0;
+//            for (Map<String, Object> map : carList) {
+//                if (isThare(i, map)) carCount++;
+//            }
+//            priceSum += priceList.get(carCount) * carCount;
+//            if(log) System.out.println("price : " + priceList.get(carCount) + " * carCount : " + carCount + " = priceSum : " + priceSum);
+//        }
+//
+//        System.out.println(priceSum);
+//    }
+//
+//    private static int maxEndTime(List<Map<String, Object>> list) {
+//        int maxTime = MIN_TIME;
+//        for (Map<String, Object> map : list) {
+//            int time = (int) map.get("endTime");
+//            if(maxTime < time) maxTime = time;
+//        }
+//        return maxTime;
+//    }
+//
+//    private static int minStartTime(List<Map<String, Object>> list) {
+//        int minTime = MAX_TIME;
+//        for (Map<String, Object> map : list) {
+//            int time = (int) map.get("startTime");
+//            if(minTime > time) minTime = time;
+//        }
+//        return minTime;
+//    }
+//
+//    private static boolean isThare(int time, Map<String, Object> map) {
+//        return (time >= (int) map.get("startTime") && time < (int) map.get("endTime"));
+//    }
 }
